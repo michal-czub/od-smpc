945d34e1f2e09a6b0811ab1eff4b918cb01bf822
lobby150 <rejwen10@interia.pl>
2020-11-21 12:00:45 +0100

    changed generating keys

---
+++ AdminApp/src/main/java/com/auctionapp/auction/modules/Keys.java
@@ -0,0 +1,44 @@
+package com.auctionapp.auction.modules;
+
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.List;
+import java.util.Random;
+
+public class Keys {
+    private final static SecureRandom random = new SecureRandom();
+    private final static BigInteger one = new BigInteger("1");
+    public BigInteger privateKey;
+    public BigInteger publicKey;
+    public BigInteger n;
+    BigInteger phi;
+    BigInteger p;
+    BigInteger q;
+    public Keys()
+    {
+        this.p = BigInteger.probablePrime(5,random);
+        this.q = BigInteger.probablePrime(5,random);
+
+        this.phi = (p.subtract(one)).multiply(q.subtract(one));
+
+        this.n = p.multiply(q);
+        this.publicKey = new BigInteger("467");
+        this.privateKey = publicKey.modInverse(phi); //d = (publicKey^-1) * mod(phi)
+
+    }
+
+    public String getE()
+    {
+        return publicKey.toString();
+    }
+    public String getN()
+    {
+        return n.toString();
+    }
+    public String getD()
+    {
+        return privateKey.toString();
+    }
+
+}
