01626d89a9ee6758905b06f83c70fa38bfcc3499
michal-czub <michalczub@o2.pl>
2020-11-25 12:46:23 +0100

    new method millionaireProblem

--- AdminApp/src/main/java/com/auctionapp/auction/modules/auction/AuctionService.java
+++ AdminApp/src/main/java/com/auctionapp/auction/modules/auction/AuctionService.java
@@ -182,12 +182,111 @@ public class AuctionService {
     }
     }
     private void millionaireProblem(Bidder firstBidder, Bidder secondBidder) {
-//        Pairs firstBidderPairs = pairsRepository.findById(firstBidder.getBidderId()).orElseThrow(); //dostep do par pierwszego biddera
-//        Pairs secondBidderPairs = pairsRepository.findById(secondBidder.getBidderId()).orElseThrow(); // dostep do par drugiego biddera
-//        BigInteger firstBidderE = new BigInteger(firstBidderPairs.getE());
-//        BigInteger firstBidderD = new BigInteger(AES.decrypt(firstBidder.getPrivateKey(),firstBidder.getSurname()));
-//        BigInteger firstBidderN = new BigInteger(firstBidderPairs.getN());
-//        //tu jakas metoda obliczajaca dana wartosc czy petla
+        // Alicia public key (e, n) as pair
+        Pairs firstBidderPairs = pairsRepository.findById(firstBidder.getBidderId()).orElseThrow();
+        // Alicia: public key - e
+        BigInteger firstBidderE = new BigInteger(firstBidderPairs.getE());
+        // Alicia: public key - n
+        BigInteger firstBidderN = new BigInteger(firstBidderPairs.getN());
+        // Alicia: private key - d
+        BigInteger firstBidderD = new BigInteger(AES.decrypt(firstBidder.getPrivateKey(),firstBidder.getSurname()));
 
+        // Alicia value: I
+        BigInteger firstBidderValue = new BigInteger(AES.decrypt(firstBidder.getValueOfBid(), firstBidder.getSurname()));
+        // Bob value: J
+        BigInteger secondBidderValue = new BigInteger(AES.decrypt(secondBidder.getValueOfBid(), secondBidder.getSurname()));
+
+        // MAX bid
+        BigInteger MAX = new BigInteger("50");
+
+        System.out.println("Value of n:  " + firstBidderN);//todo delete
+        System.out.println("Value of e:  " + firstBidderE);//todo delete
+        System.out.println("Value of d:  " + firstBidderD);//todo delete
+        System.out.println("Alicia value:  " + firstBidderValue);//todo delete
+        System.out.println("Bob value:  " + secondBidderValue); //todo delete
+
+        // BOB ===============================================
+        BigInteger X = new BigInteger("3220");
+        System.out.println("Value of X:  " + X);    //todo delete
+
+        BigInteger C;
+        C = X.modPow(firstBidderE, firstBidderN);
+        System.out.println("Value of C:  " + C);    //todo delete
+
+        BigInteger m = C.subtract(secondBidderValue).add(BigInteger.ONE);
+        System.out.println("Value of m:  " + m);    //todo delete
+        // ==================================================
+
+        // ALICIA ===========================================
+        // Alicia inserts MAX number of values to the list Y[]
+        ArrayList<BigInteger> Y = new ArrayList<BigInteger>();
+        for (BigInteger j = BigInteger.valueOf(0); j.compareTo(MAX) < 0; j = j.add(BigInteger.ONE))
+        {
+            BigInteger m1 = m.add(j);
+            BigInteger modpow = m1.modPow(firstBidderD, firstBidderN);
+            Y.add(modpow);
+        }
+        // if j = J, Alicia should get value of X
+
+        ArrayList<BigInteger> Z = new ArrayList<BigInteger>();
+        BigInteger P;
+
+        while(true)
+        {
+            BigInteger Pt = new BigInteger(7, 1, new Random());
+            if (Pt.isProbablePrime(1))
+            {
+                System.out.println("Value of P:  " + Pt);   //  todo delete
+                P = Pt;
+                break;
+            }
+        }
+
+        System.out.println("Bit length of P:  " + P.bitLength());   // todo delete
+
+        for (BigInteger j = BigInteger.valueOf(0); j.compareTo(MAX) < 0; j = j.add(BigInteger.ONE))
+        {
+            Z.add(Y.get(j.intValue()).mod(P));
+            System.out.println(j.add(BigInteger.ONE) + "Value of Z[" + j + "]:  " + Z.get(j.intValue()));       // todo delete
+        }
+
+        ArrayList<BigInteger> W = new ArrayList<BigInteger>();
+        for (BigInteger i = BigInteger.valueOf(0); i.compareTo(MAX) < 0; i = i.add(BigInteger.ONE))
+        {
+            int result = i.compareTo((firstBidderValue.subtract(BigInteger.ONE)));
+            System.out.println("Value of RESULT:  " + result);      // todo delete
+            if (result >= 0)
+            {
+                W.add((Z.get(i.intValue())).add(BigInteger.ONE));
+            }
+            else
+            {
+                W.add(Z.get(i.intValue()));
+            }
+
+            System.out.println(i.add(BigInteger.ONE) + "Value of W[" + i + "]:  " + W.get(i.intValue())); // todo delete
+        }
+
+        // BOB ===========================================================
+        //Bob checks if W[j] === X mod P
+        //if not then Alicia started incrementing values of Z[j] for indexes smaller than J
+        //which means I < J, which means Bob bidded more than Alicia
+        //================================================================
+
+        BigInteger G = X.mod(P);
+        System.out.println("Value of G:  " + G);    // todo delete
+
+        int result = G.compareTo(W.get(secondBidderValue.intValue()-1));
+        if (result == 0)
+        {
+            System.out.println("Alicia bidded more (or same)!");// todo delete
+            firstBidder.setWonMillionaireProblemCount(firstBidder.getWonMillionaireProblemCount()+1);
+            bidderRepository.save(firstBidder);
+        }
+        else
+        {
+            secondBidder.setWonMillionaireProblemCount(secondBidder.getWonMillionaireProblemCount()+1);
+            bidderRepository.save(secondBidder);
+        }
     }
 }
